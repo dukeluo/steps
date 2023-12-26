2beb77085694c09623a753ce734f9e7919ec7a4b
Huan Luo <dukeluo@outlook.com>
2023-12-26 20:45:46 +0800

    feat: update style for the step page

--- apps/website/app/steps/[slug]/page.tsx
+++ apps/website/app/steps/[slug]/page.tsx
@@ -1,17 +1,18 @@
 import { allSteps, Step } from 'contentlayer/generated'
 import parse from 'html-react-parser'
+import { notFound } from 'next/navigation'
 
 export default function Step({ params }: Readonly<{ params: { slug: string } }>) {
   const { slug } = params
 
   const step = allSteps.find((s: Readonly<Step>) => s.url.includes(slug))
 
-  if (!step) return null
+  if (!step) return notFound()
 
   return (
-    <div>
+    <article className="max-w-sm md:max-w-3xl lg:max-w-4xl mx-auto prose">
       <h1>{step.title}</h1>
-      <article className="prose">{parse(step.body.html)}</article>
-    </div>
+      {parse(step.body.html)}
+    </article>
   )
 }
